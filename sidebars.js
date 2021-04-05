a95d7b52b535b1bab4b024d09f7a5c19bd109228
wooodhead <jcwsw129@gmail.com>
2021-04-05 09:53:43 +0800

    save

---
+++ sidebars.js
@@ -0,0 +1,26 @@
+const { getFilesOf, getDirectoriesOf, removePriority } = require('./src/utils');
+const { CATEGORY_SLUGS, SUB_CATEGORY_SLUGS } = require('./src/constants');
+
+const categories = getDirectoriesOf('docs');
+const getSubCategories = category => getDirectoriesOf(`docs/${category}`);
+
+const createSubCategoryItem = (category, subCategory) => ({
+  type: 'category',
+  label: SUB_CATEGORY_SLUGS[removePriority(subCategory)],
+  items: getFilesOf(`docs/${category}/${subCategory}`).map(
+    fileName => `${category}/${subCategory}/${fileName}`,
+  ),
+});
+
+console.log('categories', categories);
+const sidebarData = categories.reduce(
+  (sidebars, category) => ({
+    ...sidebars,
+    [category]: getSubCategories(category).map(subCategory =>
+      createSubCategoryItem(category, subCategory),
+    ),
+  }),
+  {});
+
+console.log('sidebarData', sidebarData);
+module.exports = sidebarData
